c70a281f5661f82e177c5487b37c9866f26ac828
Isai <daisai01228@hotmail.com>
2021-08-03 16:46:55 -0500

    progress

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const images = {
     logo: "./images/logo.jpg",
     backg: "./images/backg.jpeg",
     hero: "./images/hero.png",
-    trunk: "./images/trunk.png"
+    trunk: "./images/trunk.png",
+    coral: "./images/Coral2.png"
 }
 class Background {
     constructor() {
@@ -40,11 +41,9 @@ class Background {
 class Axolotl {
     constructor() {
         this.y = 50
-        this.x = 500
-        this.width = 250
-        this.height = 100
-        this.sx = 500
-        this.sy = 0
+        this.x = 400
+        this.width = 50
+        this.height = 50
         this.image = new Image()
         this.image.src = images.hero
         this.image.onload = () => {
@@ -53,8 +52,7 @@ class Axolotl {
     }
 
     draw() {
-        if (this.x < -axolotl.width) this.x = 500
-        cxt.drawImage(this.image, this.sx, this.sy, 500, 500, this.x, this.y, this.width, this.height)
+        cxt.drawImage(this.image, this.x, this.y, this.width, this.height)
     }
 
     goLeft() {
@@ -68,18 +66,6 @@ class Axolotl {
     }
     goDown() {
         this.y += 30
-    }
-    isToching(trunk) {
-        return (
-            this.x < trunk.x + trunk.width &&
-            this.x + this.width > trunk.x &&
-            this.y < trunk.y + trunk.height &&
-            this.y + this.height > trunk.y
-        )
-    }
-
-    isToching(coral) {
-
     }
 }
 
@@ -99,23 +85,50 @@ document.addEventListener("keydown", ({ keyCode }) => {
     }
 })
 
-// class Trunk {
-//     constructor() {
-//         this.x = 
-//     }
-// }
+class Trunk {
+    constructor(x) {
+        this.x = x
+        this.y = canvas.height
+        this.width = 100
+        this.height = 50
+        this.trunk = new Image()
+        this.trunk.src = images.trunk
+    }
+    draw() {
+        this.y--
+            cxt.drawImage(this.trunk, this.x, this.y, this.width, this.height)
+    }
+}
+
+function generateTrunks() {
+    if (frames % 300 === 0) {
+        max = 600
+        min = 1
+        const ventanita = 150
+        const randomWidth = Math.floor(Math.random() * (max - min))
+        obstacles.push(new Trunk(randomWidth + ventanita, randomWidth))
+        console.log(obstacles)
+    }
+}
+
+function drawTrunks() {
+    obstacles.forEach(trunk => trunk.draw())
+}
+
+const trunk = new Trunk()
 
 function gameOver() {
     clearInterval(interval)
 }
 
 function checkColition() {
-    if (axolotl.x >= canvas.width - axolotl.width) {
+    if (axolotl.x >= canvas.width - 150) {
         return gameOver()
     }
     if (axolotl.y >= canvas.height - axolotl.height) {
         return gameOver()
     }
+
 }
 
 const background = new Background()
@@ -127,6 +140,8 @@ function update() {
     background.draw()
     axolotl.draw()
     checkColition()
+    generateTrunks()
+    drawTrunks()
 }
 
 function start() {
