56c02ef9bd21ac4cc1fd3c0478adbc257c65defc
Isai <daisai01228@hotmail.com>
2021-08-05 18:49:01 -0500

    finally_Axolotl_Game

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const images = {
     coral: "./images/Coral2.png",
     villian: "./images/villian.png"
 }
+
+const music = {
+    omaeWa: "./music/omaeWa.mp3"
+}
 class Background {
     constructor() {
         this.x = 0
@@ -24,6 +28,9 @@ class Background {
         this.image.onload = () => {
             this.draw()
         }
+        this.omaeWa = new Audio()
+        this.omaeWa.src = music.omaeWa
+        this.omaeWa.loop = true
     }
     draw() {
         this.y--
@@ -41,8 +48,8 @@ class Background {
 
 class Axolotl {
     constructor() {
-        this.y = 100
-        this.x = 400
+        this.y = 200
+        this.x = 500
         this.width = 50
         this.height = 50
         this.image = new Image()
@@ -77,15 +84,25 @@ class Axolotl {
             this.y + this.height > trunk.y
         )
     }
+
+
+    istoching(villian) {
+        return (
+            this.x < villian.x + villian.width &&
+            this.x + this.width > villian.x &&
+            this.y < villian.y + villian.height &&
+            this.y + this.height > villian.y
+        )
+    }
 }
 
 document.addEventListener("keydown", ({ keyCode }) => {
     switch (keyCode) {
         case 37:
-            axolotl.goLeft()
+            axolotl.goLeft(), villian.goLeft()
             break;
         case 39:
-            axolotl.goRight()
+            axolotl.goRight(), villian.goRight()
             break;
         case 38:
             axolotl.goUp()
@@ -94,6 +111,32 @@ document.addEventListener("keydown", ({ keyCode }) => {
             axolotl.goDown()
     }
 })
+
+class Villian {
+    constructor() {
+        this.x = 470
+        this.y = -3
+        this.width = 100
+        this.height = 150
+        this.image = new Image()
+        this.image.src = images.villian
+        this.image.onload = () => {
+            this.draw()
+        }
+
+    }
+    draw() {
+        cxt.drawImage(this.image, this.x, this.y, this.width, this.height)
+    }
+
+    goLeft() {
+        this.x -= 30
+    }
+    goRight() {
+        this.x += 30
+    }
+}
+const villian = new Villian()
 
 class Trunk {
     constructor(x) {
@@ -161,6 +204,7 @@ const coral = new Coral()
 
 function gameOver() {
     clearInterval(interval)
+    background.omaeWa.pause()
 }
 
 function checkColition() {
@@ -183,6 +227,8 @@ function checkColition() {
         }
         axolotl.istoching(trunk) ? gameOver() : null
     })
+
+    axolotl.istoching(villian) ? gameOver() : null
 }
 
 const background = new Background()
@@ -193,14 +239,18 @@ function update() {
     cxt.clearRect(0, 0, canvas.width, canvas.height)
     background.draw()
     axolotl.draw()
+    villian.draw()
     checkColition()
     generateTrunks()
     drawTrunks()
     generateCoral()
     drawCoral()
+    start()
 }
 
 function start() {
+    if (interval) return;
+    background.omaeWa.play()
     interval = setInterval(update, 1000 / 60)
 }
 start()
